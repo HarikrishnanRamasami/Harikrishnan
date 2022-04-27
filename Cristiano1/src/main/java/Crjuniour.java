9e6acb04ee21d8128e36ffd8c6f35b988116a33f
HarikrishnanRamasami <104414504+HarikrishnanRamasami@users.noreply.github.com>
2022-04-27 14:14:24 +0530

    Update Crjuniour.java

--- Cristiano1/src/main/java/Crjuniour.java
+++ Cristiano1/src/main/java/Crjuniour.java
@@ -2,7 +2,7 @@
 public class Crjuniour {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+         System.out.println("2021 world best player:Crstiano Ronaldo");
       
 	}
 
