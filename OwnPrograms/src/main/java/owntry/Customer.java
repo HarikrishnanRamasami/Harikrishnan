a726063de5706cdf397abad3e0b7c269f2a4ff34
harikrishnan.r <harikrishnan.r@DT150145.QICHOAPPL.com>
2022-06-02 09:48:25 +0530

    sccess Merge branch 'master' of https://github.com/HarikrishnanRamasami/Harikrishnan.git

--- OwnPrograms/src/main/java/owntry/Customer.java
+++ OwnPrograms/src/main/java/owntry/Customer.java
@@ -2,7 +2,7 @@ package owntry;
 
 public class Customer {
 	static {
-		System.out.println("Company Name [ puma ]...!");
+		System.out.println("Company Name [ puma ]....!");
 	}
 public static void main(String[] args) {
 	ShoeShop shoe=new ShoeShop();
